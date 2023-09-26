7902fdc1d80a62b50c7927ffe0086d34cc11cfac
Alex Jr <alexjunior2018@gmail.com>
2023-09-26 11:38:59 -0300

    Corrigindo tamanho do gráfico de Barra  e Pizza

--- dashboardempresarial/src/App.js
+++ dashboardempresarial/src/App.js
@@ -5,16 +5,18 @@ import {
   CategoryScale,
   LinearScale,
   BarElement,
+  ArcElement,
   Title,
   Tooltip,
   Legend,
 } from 'chart.js';
-import { Bar } from 'react-chartjs-2';
+import { Bar, Pie } from 'react-chartjs-2';
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
   BarElement,
+  ArcElement,
   Title,
   Tooltip,
   Legend
@@ -51,6 +53,33 @@ export const data = {
           borderColor: 'blue',
           borderWidth: 1
       },
+  ],
+};
+
+export const dataPie = {
+  labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
+  datasets: [
+    {
+      label: '% de vendas',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
   ],
 };
 
@@ -193,22 +222,22 @@ export default function App() {
               </div>
               </center>
             </div>
-          
-           
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-2 mt-5">
-          <div className="bg-gray-800 p-4 rounded-lg shadow-md" style={{ width: '100%', height: '400px' }}>
+
+          <div className="flex">
+            <div className="bg-gray-800 p-4 rounded-lg shadow-md" style={{width: '100%', height: '460px', margin: 20}}>
               <h4 className="text-lg font-semibold text-gray-300">Desempenho Trimestral de Vendas</h4>
               <p className="text-gray-400 mt-2">Comparação das Vendas por Trimestre no Ano Atual</p>
               <Bar options={options} data={data} />
             </div>
-            <div className="bg-gray-800 p-4 rounded-lg shadow-md" style={{ width: '100%', height: '400px' }}>
-              <h4 className="text-lg font-semibold text-gray-300">Desempenho Anual de Vendas</h4>
-              <p className="text-gray-400 mt-2">Comparação das Vendas por Trimestre no Ano Atual</p>
-              <Bar options={options} data={data} />
+            <div className="bg-gray-800 p-4 rounded-lg shadow-md" style={{width: '50%', margin: 20 }}>
+              <h4 className="text-lg font-semibold text-gray-300">Desempenho Mensal de Vendas</h4>
+              <p className="text-gray-400 mt-2">Comparação das Vendas por Mês no Ano Atual</p>
+              <div style={{ width: '100%', height: 'auto'}}>
+                  <Pie data={dataPie} />
+              </div>
             </div>
           </div>
-          
         </main>
       </div>
     </div>
