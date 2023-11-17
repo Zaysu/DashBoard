a706d7a9b07c681ab75edea9b376e869bcec2c53
marcelo.goncalves <marcelo.goncalves@smartnx.com>
2023-11-17 11:48:08 -0300

    criando rotas

--- dashboardempresarial/src/App.js
+++ dashboardempresarial/src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
-import React from 'react';
-import Home from "./components/Home"
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import "./App.css";
+import React from "react";
+import Home from "./components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-    <Route exact path="/" element={<Home/>}/>
-    <Route exact path="/resultados" element={<Home/>}/>
-    <Route exact path="/desempenho" element={<Home/>}/>
-    <Route exact path="/tendencias" element={<Home/>}/>
-    <Route exact path="/avaliacao" element={<Home/>}/>
-    <Route exact path="/vendas-trimestre" element={<Home/>}/>
-    <Route exact path="/vendas-mensal" element={<Home/>}/>
-
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/resultados" element={<Home />} />
+        <Route exact path="/desempenho" element={<Home />} />
+        <Route exact path="/tendencias" element={<Home />} />
+        <Route exact path="/avaliacao" element={<Home />} />
+        <Route exact path="/vendas-trimestre" element={<Home />} />
+        <Route exact path="/vendas-mensais" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
